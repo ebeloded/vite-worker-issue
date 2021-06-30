37a9ab3b99333952f596454c9c3a37b9cf38f7c0
ebeloded <ebeloded@users.noreply.github.com>
2021-06-30 02:00:16 +0000

    deploy: 054a6a04ff579f1c08cbed5883af612f97a19828

--- assets/index.6e28b1e1.js
+++ assets/index.6e28b1e1.js
@@ -1,1 +1,1 @@
-new function(){new Worker("/assets/worker.fc994f6f.js",{type:"module"})},document.querySelector("#app").innerHTML='\n  <h1>Hello Vite!</h1>\n  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>\n';
+new function(){new Worker("/vite-worker-issue/assets/worker.fc994f6f.js",{type:"module"})},document.querySelector("#app").innerHTML='\n  <h1>Hello Vite!</h1>\n  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>\n';
